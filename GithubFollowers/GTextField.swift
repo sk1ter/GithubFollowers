2af0f83337c32577225b7eb8f2e0b5ab0b190476
Javlonbek <javlonbek.sharipoff@gmail.com>
2024-03-06 18:32:51 +0500

    added custom textfield and button

---
+++ GithubFollowers/GTextField.swift
@@ -0,0 +1,42 @@
+//
+//  GTextField.swift
+//  GithubFollowers
+//
+//  Created by Javlonbek Sharipov on 06/03/24.
+//
+
+import UIKit
+
+class GTextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        
+        placeholder = "Enter username"
+        
+    }
+
+}
